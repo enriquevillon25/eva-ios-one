2f8cbe5fe723a190fceda864d4606c7804fa4905
Enrique Villón <enriquevillon2597@gmail.com>
2023-07-16 17:07:50 -0600

    first commit

---
+++ evaluation-one/FinalViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FinalViewController.swift
+//  evaluation-one
+//
+//  Created by Enrique Villón on 14/07/23.
+//
+
+import UIKit
+
+class FinalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        print("entrando")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- evaluation-one/ViewController.swift
+++ evaluation-one/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var names = ["Enrique","Gracia"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cel = tableView.dequeueReusableCell(withIdentifier: "celdaTabla", for: indexPath)
+        cel.textLabel?.text = names[indexPath.row]
+        return cel
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("hola", names[indexPath.row])
+    }
+    
+    
 }
 
